7886bb42d2d2c67cb6601b4026ed56dca25f9884
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-08-17 15:01:33 +0600

    forget password token and send mail

--- app/Http/Controllers/API/Auth/ForgetPasswordController.php
+++ app/Http/Controllers/API/Auth/ForgetPasswordController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\API\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\BaseController;
+use App\Jobs\PasswordResetMailSendJob;
+use App\Mail\PasswordResetMail;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
-class ForgetPasswordController extends Controller
+class ForgetPasswordController extends BaseController
 {
     /**
      * Forget Password
@@ -21,7 +28,50 @@ class ForgetPasswordController extends Controller
             return $this->sendError('Validation Error.', $validator->errors());       
         }
 
+        $email = $request->input('email');
 
+        $user = User::where('email', $email)->first();
+        if($user){
+            $email = $user->email;
+            $reset_token = $this->generateResetToken($email);
+
+            $success = [
+                "reset_token" => $reset_token 
+            ];
+       
+            return $this->sendResponse('Password reset mail sent.', $success);
+
+        }else{
+            return $this->sendError('This email not registration yet', [], 404); 
+        }
         
     }
+
+
+    protected function generateResetToken($email){
+        $token = Str::random(20);
+        $password_reset = DB::table('password_resets')->where('email', $email)->first();
+        if($password_reset){
+            DB::table('password_resets')->where('email', $email)->update([
+                "token" => $token
+            ]);
+        }else{
+            DB::table('password_resets')->insert([
+                "email" => $email,
+                "token" => $token
+            ]);
+        }
+        $this->sendPasswordResetMail($email, $token);
+        return $token;
+    }
+
+    protected function sendPasswordResetMail($email, $token){
+        $details = [
+            'email' => $email,
+            'subject' => 'Password Reset',
+            'url' => asset("/api/v1/reset-password/$token")
+        ];
+        dispatch(new PasswordResetMailSendJob($details));
+    }
+
 }
